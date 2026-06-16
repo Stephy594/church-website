fbe42c2ad75565e834d19e8624b28c1e1350207c
Stephy Elizabeth Koshy <stephyprasobh2024@gmail.com>
2026-06-16 14:54:07 +0200

    Update mobile navbar and home section

--- src/app/pages/gallery/gallery.ts
+++ src/app/pages/gallery/gallery.ts
@@ -12,34 +12,32 @@ export class Gallery {
 
   categories = ['ALL', 'WORSHIP', 'SUNDAY SCHOOL', 'YOUTH', 'LADIES MEETING', 'EVENTS'];
 
+ heroSlides = [
+  '/images/Instagram3_files/GalSlide1.jpg',
+  '/images/Instagram3_files/GalSlide2.jpg',
+  '/images/Instagram3_files/GalSlide3.jpg',
+  '/images/Instagram3_files/GalSlide4.jpg'
+];
+
   images = [
     { src: 'images/Gallery/IMG_6735.JPG.jpeg', category: 'EVENTS' },
     { src: 'images/Gallery/IMG_6752.JPG.jpeg', category: 'EVENTS' },
-    { src: 'images/Gallery/IMG_6753.JPG.jpeg', category: 'EVENTS' },
-    { src: 'images/Gallery/IMG_6754.JPG.jpeg', category: 'EVENTS' },
 
-    { src: 'images/Gallery/IMG_6918.JPG.jpeg', category: 'EVENTS' },
     { src: 'images/Gallery/IMG_6919.JPG.jpeg', category: 'EVENTS' },
-    { src: 'images/Gallery/IMG_7020.PNG', category: 'EVENTS' },
-    { src: 'images/Gallery/IMG_7021.PNG', category: 'EVENTS' },
 
     { src: 'images/Gallery/IMG_7022.PNG', category: 'EVENTS' },
-    { src: 'images/Gallery/IMG_7023.PNG', category: 'EVENTS' },
     { src: 'images/Gallery/IMG_7901.JPG.jpeg', category: 'EVENTS' },
     { src: 'images/Gallery/IMG_7902.JPG.jpeg', category: 'EVENTS' },
 
-    { src: 'images/Gallery/IMG_7903.JPG.jpeg', category: 'LADIES MEETING' },
     { src: 'images/Gallery/IMG_7904.JPG.jpeg', category: 'LADIES MEETING' },
-    { src: 'images/Gallery/IMG_7905.JPG.jpeg', category: 'LADIES MEETING' },
     { src: 'images/Gallery/IMG_7906.JPG.jpeg', category: 'LADIES MEETING' },
 
-    { src: 'images/Gallery/IMG_8475.JPG.jpeg', category: 'WORSHIP' },
-    { src: 'images/Gallery/IMG_8476.JPG.jpeg', category: 'WORSHIP' },
-    { src: 'images/Gallery/IMG_8477.JPG.jpeg', category: 'WORSHIP' },
+    { src: 'images/Gallery/GalSlide4.jpg', category: 'WORSHIP' },
+    { src: 'images/Gallery/Worship2.jpg', category: 'WORSHIP' },
+    { src: 'images/Gallery/Worship3.jpg', category: 'WORSHIP' },
     { src: 'images/Gallery/IMG_8479.JPG.jpeg', category: 'WORSHIP' },
     { src: 'images/Gallery/IMG_8489.JPG.jpeg', category: 'WORSHIP' },
     { src: 'images/Gallery/IMG_8491.JPG.jpeg', category: 'EVENTS' },
-    { src: 'images/Gallery/IMG_8493.JPG.jpeg', category: 'EVENTS' },
     { src: 'images/Gallery/IMG_8496.JPG.jpeg', category: 'WORSHIP' },
     { src: 'images/Gallery/IMG_8497.JPG.jpeg', category: 'WORSHIP' }
   ];
